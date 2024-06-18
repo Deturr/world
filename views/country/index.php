942fdb8510fa872402239f40de20ece03152b482
Toby Emeboh <Toby.Emeboh@student.rocva.nl>
2024-06-19 00:09:35 +0200

    Grid View (2)

--- views/country/index.php
+++ views/country/index.php
@@ -26,20 +26,19 @@ $this->params['breadcrumbs'][] = $this->title;
         
         'columns' => [
            //['class' => 'yii\grid\SerialColumn'],
-            
+           [
+            'class' => ActionColumn::className(), 'contentOptions' => ['style' => 'width:100px; white-space: normal;'],
+            'urlCreator' => function ($action, Country $model, $key, $index, $column) {
+                return Url::toRoute([$action, 'Code' => $model->Code]);
+            }
+        ],
             ['label' => 'Code', 'attribute' => 'Code', 'contentOptions' => ['style' => 'width:30px; white-space: normal;'],],
-            ['label' => 'Name', 'attribute' => 'Name', 'contentOptions' => ['style' => 'width:300px; white-space: normal; font-weight: bold;'],],
+            ['label' => 'Name', 'attribute' => 'Name', 'contentOptions' => ['style' => 'width:300px; white-space: normal; font-weight: bold; color:darkblue;'],],
             ['label' => 'Hoofdstad', 'attribute' => 'Capital', 'contentOptions' => ['style' => 'width:200px; white-space: normal;'], 'format' => 'raw', 'value' => function($data) { return Html::a('Naar hoofdstad', ['/city/index', 'CitySearch[ID]' => $data->Capital]); }],
-            ['label' => 'Inwoners', 'attribute' => 'Population', 'contentOptions' => ['style' => 'width:30px; white-space: normal;'],],
-            ['label' => 'Oppervlakte', 'attribute' => 'SurfaceArea', 'contentOptions' => ['style' => 'width:30px; white-space: normal;'],'format' => 'raw','value' => function($data) { return sprintf("%8d k&#13217;", $data->SurfaceArea); }],
+            ['label' => 'Inwoners', 'attribute' => 'Population', 'value' => function($model) { if ($model->Population == 0) { return 'Onbewoond'; } else { return $model->Population; } }, 'contentOptions' => ['style' => 'width:30px; white-space: normal; text-align:right;'], 'headerOptions' => ['style' => 'text-align:right;'],],
+            ['label' => 'Oppervlakte', 'attribute' => 'SurfaceArea', 'contentOptions' => ['style' => 'width:30px; white-space: normal; text-align:right;'],'headerOptions' => [ 'style' => 'text-align:right;' ],'format' => 'raw','value' => function($data) { return sprintf("%8d k&#13217;", $data->SurfaceArea); }],
             // Nieuwe kolom voor bevolkingsdichtheid
-            ['label' => 'Bevolkingsdichtheid','value' => function ($model) {return $model->SurfaceArea > 0 ? round($model->Population / $model->SurfaceArea) : 0;},'contentOptions' => ['style' => 'width:30px; white-space: normal;'],],
-            [
-                'class' => ActionColumn::className(),
-                'urlCreator' => function ($action, Country $model, $key, $index, $column) {
-                    return Url::toRoute([$action, 'Code' => $model->Code]);
-                }
-            ],
+            ['label' => 'Bevolkingsdichtheid', 'value' => function ($model) { return $model->SurfaceArea > 0 ? round($model->Population / $model->SurfaceArea, 2) : 0; }, 'contentOptions' => ['style' => 'width:30px; white-space: normal; text-align:right;'], 'headerOptions' => ['style' => 'text-align:right;'],],
         ],
     ]); ?>
     <p>
